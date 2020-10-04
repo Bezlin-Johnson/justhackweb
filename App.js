370dd77c5276b495510b0a275d88c04f85ca7da7
bezlin6mechminerz <54895485+bezlin6mechminerz@users.noreply.github.com>
2020-10-04 15:13:51 +0530

    done

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 // /* /index.html 200
-// redirect file in netlify code
+// redirect file in netlify code cli
 import React, { Component } from "react";
 import "./App.css";
 import fire from "./fire.js";
